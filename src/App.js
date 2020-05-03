f0c3b2f5fd7ee4e74d724a9ca86e0cd8fe919b7f
Andres Martin <andres.pena@yello.co>
2020-05-02 21:30:44 -0500

    [add] add props to Person component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Martin", age: 24 },
+      { name: "Maria", age: 23 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>It's working</p>
-        <Person />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hobies</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobies</Person>
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Some text'))
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const person = () => {
-  return <p>I&apos;m a Person!</p>;
+const person = (props) => {
+  return (
+    <div>
+      <p>I&apos;m {props.name} and I am {props.age} years old!</p>
+      <p>{props.children}</p>
+    </div>
+  );
+};
+
+person.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.string
 };
 
 export default person;
