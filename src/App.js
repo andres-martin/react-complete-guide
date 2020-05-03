07af8c6e0b40dc5abf7fe7e0594652722fccee82
Andres Martin <andres.pena@yello.co>
2020-05-02 19:07:01 -0500

    [add] add eslint su[port and Person component created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
+        <p>It's working</p>
+        <Person />
       </div>
     );
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Some text'))
   }
 }
 
