5200765d396a94b7dabc05c311d47f453cff9b9e
Andres Martin <andres.pena@yello.co>
2020-05-20 22:28:27 -0500

    [add] add styles components package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+    background-color: ${props => !props.alt ? 'green' : 'red'};
+    color: white;
+    font: inherit;
+    border: 1px solid blue;
+    padding: 8px;
+    cursor: pointer;
+
+    &:hover {
+      background-color: ${props => !props.alt ? 'lightgreen' : 'salmon'};
+      color: black;
+    }
+`;
 
 class App extends Component {
   state = {
     persons: [
       { id: '001', name: "Martin", age: '24' },
-      { id: '002', name: "Maria", age: '23' }
+      { id: '002', name: "Maria", age: '23' },
+      { id: '003', name: "Andres", age: '23' }
     ],
     showPersons: false
   }
@@ -47,11 +63,34 @@ class App extends Component {
   )
 
   render() {
+    // const style = {
+    //   backgroundColor: !this.state.showPersons ? 'green' : 'red',
+    //   color: 'white',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer',
+    //   ':hover': {
+    //     backgroundColor: !this.state.showPersons ? 'lightgreen' : 'salmon',
+    //     color: 'black'
+    //   }
+    // }
+
+    const classes = []
+
+    if (this.state.persons.length <= 2) classes.push('red');
+    if (this.state.persons.length <= 1) classes.push('bold');
+
     return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
-        <p>It's working</p>
-        <button onClick={this.togglePersonsHandler}>Switch Name</button>
+        <p className={classes.join(' ')}>It's working</p>
+        <StyledButton
+          alt={this.state.showPersons}
+          onClick={this.togglePersonsHandler}
+        >
+          Toggle Persons
+        </StyledButton>
         {this.state.showPersons && this.showPersonsFragment(this.state.persons)}
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Person.css'
+import styled from 'styled-components';
+
+// import './Person.css'
+
+const Div = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+
+
+    @media (min-width: 500px) {
+      width: '450px'
+    }
+`;
 
 const person = (props) => {
+
   return (
-    <div className="Person">
+    <Div>
       <p onClick={props.click}>I&apos;m {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </div>
+    </Div>
   );
 };
 
 person.propTypes = {
   name: PropTypes.string,
   age: PropTypes.string
-};
+}
 
 export default person;
